7021da5809bd58b265d46830e2c1061953a031cd
Kamakshi <kamakshipandoh8@gmail.com>
2024-02-03 23:13:03 +0530

    compleetd

---
+++ forms/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MulInputs from './components/mulInputs'
+
+const App = () => {
+  return (
+    <div>
+      <MulInputs />
+    </div>
+  )
+}
+
+export default App
